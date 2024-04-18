f19a119f00529bd450da69b77f0f28c6e95741a0
Ezz dghedy <ezzmohsend@gmail.com>
2024-04-18 18:53:36 +0200

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import { FooterFirstColumn, FooterLinks } from '../subComponent';
 import { footerAddress, footerCompany, footerShop } from './../assets/index';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa6';
@@ -9,7 +10,7 @@ const Footer = () => {
   return (
     <div className="flex flex-col">
       {/* First Section */}
-      <div className="flex flex-col lg:flex-row px-2 py-6 md:px-4 lg:px-6 justify-start lg:justify-between">
+      <div className="bg-[#fafafa] flex flex-col lg:flex-row px-2 py-6 md:px-4 lg:px-6 justify-start lg:justify-between">
         {/* First column */}
         <FooterFirstColumn />
         {/* Second Column */}
@@ -21,20 +22,49 @@ const Footer = () => {
           <div className="flex flex-col gap-4">
             <h2 className="capitalize font-man text-[#1b1b1b]">follow us</h2>
             <ul className="font-man font-light text-[#1b1b1b] ">
-              <li className="my-2 flex capitalize justify-between items-center">
-                <FaFacebookF className="mr-2" />
-                facebook
-              </li>
-              <li className="my-2 flex capitalize justify-between items-center">
-                <FaInstagram className="mr-2" />
-                instagram
-              </li>
-              <li className="my-2 flex capitalize justify-between items-center">
-                <FaXTwitter className="mr-2 " />
-                <span className='mr-6'>twitter</span>
-              </li>
+              <Link to="">
+                <li className="my-2 flex capitalize justify-between items-center">
+                  <FaFacebookF className="mr-2" />
+                  facebook
+                </li>
+              </Link>
+              <Link to="">
+                <li className="my-2 flex capitalize justify-between items-center">
+                  <FaInstagram className="mr-2" />
+                  instagram
+                </li>
+              </Link>
+              <Link to="">
+                <li className="my-2 flex capitalize justify-between items-center">
+                  <FaXTwitter className="mr-2 " />
+                  <span className="mr-6">twitter</span>
+                </li>
+              </Link>
             </ul>
           </div>
+        </div>
+      </div>
+      {/* Second Section */}
+      <div className="bg-[#000] my-2 flex flex-col md:flex-row-reverse px-2 gap-4 py-6 md:px-4 lg:px-6 justify-start lg:justify-between">
+        <div className="flex flex-col md:flex-row justify-evenly w-1/2 gap-4">
+          <Link to="">
+            <h2 className="text-[#F5F5F5B2] font-man font-bold capitalize">
+              privacy Policy
+            </h2>
+          </Link>
+          <Link to="">
+            <h2 className="text-[#F5F5F5B2] font-man font-bold capitalize">
+              terms of services
+            </h2>
+          </Link>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between w-1/2 gap-4">
+          <h6 className="text-[white] text-sm font-man font-bold capitalize">
+            © 2022 Habitat Scapes, LLC
+          </h6>
+          <h6 className="text-[white] text-sm font-man font-bold capitalize">
+            Crafted with ♥️ by HighNorth
+          </h6>
         </div>
       </div>
     </div>
--- src/subComponent/FooterLinks.jsx
+++ src/subComponent/FooterLinks.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom";
+
 const FooterLinks = ({title,list}) => {
   return (
     <div className={`flex flex-col gap-4 ${title=="Address"?"w-1/5":""}`}>
@@ -8,7 +10,7 @@ const FooterLinks = ({title,list}) => {
             </h2>
             <ul className="font-man font-light text-[#1b1b1b] ">
               {list.map((li) => {
-                return <li key={li} className='my-2'>{li}</li>;
+                return <Link key={li} to="">  <li className='my-2'>{li}</li></Link>;
               })}
             </ul>
           </div>
